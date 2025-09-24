a7f71e04671738a8eeb19ab260f3fa3de97afad8
Yifan-Q <ivanqiu0616@gmail.com>
2025-09-24 17:52:15 -0400

    Implemented Retrieve Method

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -88,8 +88,16 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        // TODO implement this method
-        return -1;
+        if (path.isEmpty()) {
+            return this.root;
+        }
+
+        if (path.charAt(0) == 'l'){
+            return this.left.retrieve(path.substring(1));
+        } else if (path.charAt(0) == 'r') {
+            return this.right.retrieve(path.substring(1));
+        } else {
+            throw new IllegalArgumentException("Illegal path");
     }
 
     /** Read in the NumberTriangle structure from a file.
